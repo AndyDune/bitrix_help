8b70086b2c195c0ff542e54f72eb285abfde9b42
Andrey Ryzhov <dune@rznlf.ru>
2015-08-26 14:46:24 +0300

    Примеры правил для ЧПУ битрикса

---
+++ urlrewrite_terms.php
@@ -0,0 +1,25 @@
+```php
+    array(
+        "CONDITION"	=>	"#^/test/([0-9]+).html#",
+        "RULE"	=>	"id=$1",
+        "PATH"	=>	"/test/page.php",
+    ),
+
+    array(
+        "CONDITION"	=>	"#^/test/([-_a-z0-9]+).([0-9]+).html#",
+        "RULE"	=>	"code=$1&id=$2",
+        "PATH"	=>	"/test/page.php",
+    ),
+
+    array(
+        "CONDITION"	=>	"#^/test/([-_a-z0-9]+)/page/([0-9]+)#",
+        "RULE"	=>	"section=$1&page=$2",
+        "PATH"	=>	"/test/page.php",
+    ),
+
+    array(
+        "CONDITION"	=>	"#^/test/([-_a-z0-9]+)$#",
+        "RULE"	=>	"section=$1",
+        "PATH"	=>	"/test/page.php",
+    ),
+```
